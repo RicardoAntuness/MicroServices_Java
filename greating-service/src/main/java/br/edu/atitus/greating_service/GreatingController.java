c85c9ef3c8a182b996ff723768c32882a83c3845
RicardoAntuness <ricardo_rantunes22@hotmail.com>
2025-04-11 21:01:38 -0300

    config-service

--- greating-service/src/main/java/br/edu/atitus/greating_service/GreatingController.java
+++ greating-service/src/main/java/br/edu/atitus/greating_service/GreatingController.java
@@ -12,20 +12,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/greating-service")
 public class GreatingController {
 	
-	@Value("${greating-service.greating}")
-	private String greating;
+//	@Value("${greating-service.greating}")
+//	private String greating;
+//	
+//	@Value("${greating-service.default-name}")
+//	private String defaultName;
 	
-	@Value("${greating-service.default-name}")
-	private String defaultName;
+	private final GreatingConfig greatingConfig;
 	
+	public GreatingController(GreatingConfig greatingConfig) {
+	super();
+	this.greatingConfig = greatingConfig;
+}
+
 	@GetMapping({"", "/", "/{namePath}"})
 	public ResponseEntity<String> getGreating(
 			@PathVariable(required = false) String namePath,
 			@RequestParam(required = false) String name
 			) {
 		if (name == null)
-			name = namePath != null ? namePath : "defaultName";
-		String returnText = String.format("%s %s!", greating, name);
+			name = namePath != null ? namePath : greatingConfig.getDefaultName();
+		String returnText = String.format("%s %s!", greatingConfig.getGreating(), name);
 		return ResponseEntity.ok(returnText);
 	}
 }
