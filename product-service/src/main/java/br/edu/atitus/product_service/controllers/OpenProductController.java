80d8ef892b02a0021e0c3131b5ab13c6bb4830cf
RicardoAntuness <ricardo_rantunes22@hotmail.com>
2025-05-28 22:26:19 -0300

    Alterações na product

--- product-service/src/main/java/br/edu/atitus/product_service/controllers/OpenProductController.java
+++ product-service/src/main/java/br/edu/atitus/product_service/controllers/OpenProductController.java
@@ -46,9 +46,14 @@ public class OpenProductController {
 			product.setConvertedPrice(product.getPrice());
 		} else {
 			CurrencyResponse currency = currencyClient.getCurrency(product.getPrice(),product.getCurrency(),targetCurrency);
-			product.setConvertedPrice(currency.getConvertedValue());
-			product.setEnvironment(product.getEnvironment() + " - " + currency.getEnvironment());
-		}
+			if(currency != null) {
+				product.setConvertedPrice(currency.getConvertedValue());
+				product.setEnvironment(product.getEnvironment() + " - " + currency.getEnvironment());
+				} else {
+					product.setConvertedPrice(-1);
+					product.setEnvironment(product.getEnvironment() + " - Currency unavalaible");
+				}
+			}
 		return ResponseEntity.ok(product);
 	}
 
