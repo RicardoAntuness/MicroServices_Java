db52bfb26de2d0bcc98220c602419ab1170aa0ec
RicardoAntuness <ricardo_rantunes22@hotmail.com>
2025-05-30 21:17:58 -0300

    Alterações gateway

---
+++ gateway-service/src/main/java/br/edu/atitus/gateway_service/configs/GatewayConfigs.java
@@ -0,0 +1,27 @@
+package br.edu.atitus.gateway_service.configs;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfigs {
+	
+	@Bean
+	RouteLocator getRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+		.route(p -> p
+				.path("/get")
+				.filters(f -> f
+						.addRequestHeader("X-USER-ID", "1234")
+						.addRequestParameter("env", "dev"))
+				.uri("http://httpbin.org"))
+		.route(p -> p
+				.path("/currency/**").uri("lb://currency-service/"))
+				.route(p -> p
+						.path("/products/**")
+						.uri("lb://product-service"))
+				.build();
+	}
+}
